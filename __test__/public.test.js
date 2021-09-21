84fc569b58304767374e430c61276856346672b9
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-09-21 15:40:55 -0700

    passing badfile

--- __test__/public.test.js
+++ __test__/public.test.js
@@ -1,18 +1,32 @@
 const  request  = require('supertest');
 const  app  = require('../lib/app.js');
 const  { readFile }  = require('fs/promises');
-const  path  = require('path');
-
+// jest.setTimeout(10000);
 
 describe('get static routes, return html from get', () => {
   it('should return index.html from GET /', async () => {
-    const filePath = path.join('./public/', 'index.html');
-    console.log(await readFile(filePath, 'utf-8'));
+    
     const [res, index] = await Promise.all(
       [
         request(app).get('/'),
-        readFile(filePath, 'utf-8')
+        readFile('./public/index.html', 'utf-8')
       ]); 
     expect(res.text).toEqual(index);
   });
+
+  it('should return main.css from GET /CSS/main.css', async () => {
+    const [res, index] = await Promise.all([
+      request(app).get('/CSS/main.css'),
+      readFile('./public/CSS/main.css', 'utf-8')
+    ]);
+    expect(res.text).toEqual(index);
+  });
+
+  it('should return 404 from GET /bad-file', async () => {
+    const res = await request(app).get('/bad-file');
+    //   readFile('./public/badfile', 'utf-8');
+    // console.log(res.statusCode);
+    expect(res.statusCode).toEqual(404);
+  });
+
 });
